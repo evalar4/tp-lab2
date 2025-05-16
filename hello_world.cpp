e2a672e8fe65ac38cc781a84275f3f18d6ebd9d8
evalar4 <evelinka-07@mail.ru>
2025-05-16 16:20:39 +0300

    updated hello world file

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
 import <iostream>
+import <string>
 using namespace std;
 int main() {
-  cout << "Hello world!" << endl;
+  string name;
+  cin >> name;
+  cout << "Hello world from " << name  << endl;
   return 0;
 }
